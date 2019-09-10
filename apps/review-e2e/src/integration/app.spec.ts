3c5542b1c94a929b8c18dcab2d89abb644c0bf7d
Isaac Mann <isaacplmann@gmail.com>
2019-09-10 16:13:31 -0400

    Create review list view

--- apps/review-e2e/src/integration/app.spec.ts
+++ apps/review-e2e/src/integration/app.spec.ts
@@ -1,9 +1,20 @@
-import { getGreeting } from '../support/app.po';
-
-describe('review', () => {
+describe('review game list', () => {
   beforeEach(() => cy.visit('/'));
 
-  it('should display welcome message', () => {
-    getGreeting().contains('Welcome to review!');
+  it('should display a list of games', () => {
+    cy.contains('Board Game Hoard: Review');
+
+    cy.contains('a', 'Settlers in the Can').within(() => {
+      cy.contains('/5');
+      cy.contains('$35.00');
+    });
+    cy.contains('a', 'Chess Pie').within(() => {
+      cy.contains('/5');
+      cy.contains('$15.00');
+    });
+    cy.contains('a', 'Purrfection').within(() => {
+      cy.contains('/5');
+      cy.contains('$45.00');
+    });
   });
 });
--- apps/review/src/app/app.tsx
+++ apps/review/src/app/app.tsx
@@ -1,25 +1,101 @@
 import React, { useEffect, useState } from 'react';
-import { Message } from '@bghoard/api-interfaces';
+import { Game } from '@bghoard/api-interfaces';
+import styled from '@emotion/styled';
+
+export const TileGrid = styled.div`
+  display: flex;
+  flex-flow: wrap;
+  justify-content: space-evenly;
+`;
+
+export const Tile = styled.div`
+  border: solid 1px #ccc;
+  border-radius: 5px;
+  overflow: hidden;
+  display: block;
+  min-width: 200px;
+  color: inherit;
+  text-decoration: none;
+  margin: 0.5em;
+  &:hover {
+    border-color: #5159ea;
+  }
+`;
+
+export const ImageWrapper = styled.div`
+  width: 200px;
+  height: 200px;
+  overflow: hidden;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-bottom: solid 1px #ccc;
+`;
+
+export const Image = styled.img`
+  max-width: 100%;
+  min-height: 100%;
+`;
+
+export const TileTitle = styled.h2`
+  text-align: center;
+`;
+
+export const TileLeftCorner = styled.div`
+  display: block;
+  float: left;
+  margin: -0.5em 0 1em 0.5em;
+`;
+
+export const TileRightCorner = styled.div`
+  display: block;
+  float: right;
+  margin: -0.5em 0.5em 1em 0;
+`;
+export const currencyFormat = (amount: number) => '$' + amount.toFixed(2);
+export const ratingFormat = (rating: number) =>
+  (rating === undefined ? '?' : rating.toFixed(0)) + '/5';
 
 export const App = () => {
-  const [m, setMessage] = useState<Message>({ message: '' });
+  const [games, setGames] = useState<Game[]>([]);
 
   useEffect(() => {
-    fetch('/api')
+    fetch('/api/game')
       .then(r => r.json())
-      .then(setMessage);
+      .then(setGames);
   }, []);
 
   return (
     <>
       <div style={{ textAlign: 'center' }}>
-        <h1>Welcome to review!</h1>
-        <img
-          width="450"
-          src="https://raw.githubusercontent.com/nrwl/nx/master/nx-logo.png"
-        />
+        <h1>Board Game Hoard: Review</h1>
       </div>
-      <div>{m.message}</div>
+      <TileGrid>
+    {games.map(game => {
+      return (
+        <a
+          href={'/' + game.id}
+          key={game.id}
+          style={{ textDecoration: 'none', color: 'inherit' }}
+        >
+          <Tile>
+            {game.image && (
+              <ImageWrapper>
+                <Image src={game.image} />
+              </ImageWrapper>
+            )}
+            <TileTitle>{game.name}</TileTitle>
+            <TileLeftCorner>
+              {ratingFormat(game.rating)}
+            </TileLeftCorner>
+            <TileRightCorner>
+              {currencyFormat(game.price)}
+            </TileRightCorner>
+          </Tile>
+        </a>
+      );
+    })}
+  </TileGrid>
     </>
   );
 };
