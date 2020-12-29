b08b09915ae607a7fa7b738934e91e9af959a51f
Isaac Mann <isaacplmann+git@gmail.com>
2020-12-29 10:23:22 -0500

    refactor add-license

--- tools/generators/add-license/index.ts
+++ tools/generators/add-license/index.ts
@@ -1,19 +1,10 @@
-import {
-  generateFiles,
-  readProjectConfiguration,
-  Tree
-} from '@nrwl/devkit';
+import { Tree, readProjectConfiguration, generateFiles } from '@nrwl/devkit';
 import { join } from 'path';
 
-export default async function(tree: Tree, schema: any) {
-    console.debug('adding LICENSE.txt to library');
+export default async function (host: Tree, schema: any) {
+  console.log('adding LICENSE.txt to library');
 
-    generateFiles(
-      tree,
-      join(__dirname, 'files'),
-      readProjectConfiguration(tree, schema.name).root,
-      {}
-    );
-
-    return;
+  const filesPath = join(__dirname, 'files');
+  const destinationPath = readProjectConfiguration(host, schema.name).root;
+  generateFiles(host, filesPath, destinationPath, {});
 }
