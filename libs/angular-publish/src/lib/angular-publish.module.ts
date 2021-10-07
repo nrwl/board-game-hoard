ecd39952904471451e6255a29c92020fd5b9391f
Isaac Mann <isaacplmann+git@gmail.com>
2021-10-07 14:21:08 -0400

    chore: add a comment

--- libs/angular-publish/src/lib/angular-publish.module.ts
+++ libs/angular-publish/src/lib/angular-publish.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @NgModule({
-  imports: [CommonModule]
+  imports: [CommonModule],
 })
 export class AngularPublishModule {}
+
+// modified
